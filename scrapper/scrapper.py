f8743fa957d12ac1efa9a82d094841df4fc91cff
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-30 12:21:47 -0300

    Add function to scrape data from static site

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,10 +1,14 @@
 import re
+from bs4 import BeautifulSoup
 
 COORDINATES_REGEX = (r'(\d{1,2}\.\d{1,4})°\s([SN]),\s'
                      r'(\d{1,3}\.\d{1,4})°\s([EW])')
 
 
 def parse_coordinates(coordinates):
+    """
+    Returns the parsed coordinates to be used as query string.
+    """
     search_results = re.search(COORDINATES_REGEX, coordinates)
     groups_found = search_results.groups()
 
@@ -17,3 +21,25 @@ def parse_coordinates(coordinates):
                           f'&{longitude_hemisphere}={longitude_angle}')
 
     return parsed_coordinates
+
+
+def scrape_data(html):
+    """
+    Returns a list of dictionaries with the parsed cities coordinates and wind info.
+    """
+    soup = BeautifulSoup(html, features='html.parser')
+    table = soup.find('table')
+    table_rows = table.find_all('tr')
+
+    data = []
+
+    for tr in table_rows[1:]:  # skip table header
+        tds = tr.find_all('td')
+
+        coordinates = parse_coordinates(tds[0].text)
+        wind = tds[1].text
+
+        data.append({'coordinates': coordinates,
+                     'wind': wind})
+
+    return data
--- scrapper/test_scrapper.py
+++ scrapper/test_scrapper.py
@@ -1,4 +1,6 @@
-from scrapper import parse_coordinates
+from scrapper import parse_coordinates, scrape_data
+
+MOCKED_SITE_FILE_PATH = 'mocks/mocked_static_site.html'
 
 
 def test_parse_coordinates():
@@ -7,3 +9,22 @@ def test_parse_coordinates():
     parsed_coordinates = parse_coordinates(coordinates)
 
     assert parsed_coordinates == 's=33.8688&e=151.2093'
+
+
+def test_scrape_data():
+    with open(MOCKED_SITE_FILE_PATH, 'r', encoding='utf-8') as site_html:
+        scraped_data = scrape_data(site_html)
+
+        assert len(scraped_data) == 10
+
+        assert scraped_data[0].get('coordinates') == 'n=51.5074&w=0.1278'
+        assert scraped_data[0].get('wind') == '47 kph'
+
+        assert scraped_data[1].get('coordinates') == 'n=41.7151&e=44.827'
+        assert scraped_data[1].get('wind') == '14 kph'
+
+        assert scraped_data[4].get('coordinates') == 's=54.8019&w=68.3030'
+        assert scraped_data[4].get('wind') == '15 kph'
+
+        assert scraped_data[9].get('coordinates') == 's=1.2921&e=36.8219'
+        assert scraped_data[9].get('wind') == '20 kph'
