2311371c6bd5e93a2c61d44f6cf7becab607886d
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-31 13:16:23 -0300

    Add get method for weather observations

--- resources/weather_observation_resource.py
+++ resources/weather_observation_resource.py
@@ -2,10 +2,16 @@ from flask_restful import Resource, request
 from flask_sqlalchemy import SQLAlchemy
 
 from database.db import db
+from models.weather_observation_model import WeatherObservation
 from schemas.weather_observation_schema import WeatherObservationSchema
 
 
 class WeatherObservationResource(Resource):
+    def get(self):
+        observations = WeatherObservation.query.all()
+
+        return WeatherObservationSchema().dump(observations, many=True)
+
     def post(self):
         json = request.get_json()
         new_observation = WeatherObservationSchema().load(json)
