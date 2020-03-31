8eaf1f7b6ac03a695391c593d88e127710e154de
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-31 10:44:50 -0300

    Add weather observation model

---
+++ models/weather_observation_model.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from application import db
+
+
+class WeatherObservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=False)
+    datetime = db.Column(db.DateTime,
+                         nullable=False, default=datetime.now)
+
+    def __repr__(self):
+        return f'<WeatherObservation {self.id} {self.city} {self.text}>'
