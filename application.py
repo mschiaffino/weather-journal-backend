7f1773f451178cacddf30cf1329445c4a42beb80
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-31 10:31:11 -0300

    Handle CORS

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from resources.cities_resource import CitiesResource
 
 application = Flask(__name__)
 api = Api(application)
+CORS(application)
 
 api.add_resource(CitiesResource, '/cities')
 
